78c8ccfe6388e61edeff045aa75d10158afea65d
Dawid Ochal <dawid.ochal@hotmail.com>
2020-08-10 20:14:51 +0100

    Add project files.

---
+++ Compare2/ProcessImage.cs
@@ -0,0 +1,47 @@
+﻿using Accord.Imaging;
+using Accord.Imaging.Filters;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compare2
+{
+    public static class ProcessImage
+    {
+        public static void Similarity(System.IO.Stream model, System.IO.Stream observed,
+            out double similPercent)
+        {
+            var bitModel = new Bitmap(model);
+            var bitObserved = new Bitmap(observed);
+
+            int modelPoints = 0, matchingPoints = 0;
+            
+            Accord.Math.Random.Generator.Seed = 0;
+
+            using (SpeededUpRobustFeaturesDetector detector = new SpeededUpRobustFeaturesDetector(threshold: 0.0002f, octaves: 5, initial: 2))
+            {
+                List<SpeededUpRobustFeaturePoint> surfModel = detector.ProcessImage(bitModel);
+                modelPoints = surfModel.Count();
+                List<SpeededUpRobustFeaturePoint> surfObserved = detector.ProcessImage(bitObserved);
+
+                KNearestNeighborMatching matcher = new KNearestNeighborMatching(5);
+                var results = matcher.Match(surfModel, surfObserved);
+                matchingPoints = results.Length;
+            }
+            // Determine if they represent the same points
+            // Obtain the pairs of associated points, we determine the homography matching all these pairs
+
+
+            // Compare the results, 0 indicates no match so return false
+            if (matchingPoints <= 0)
+            {
+                similPercent = 0.0f;
+            }
+
+            similPercent = ((double)matchingPoints * 100) / (double)modelPoints;
+        }
+    }
+}
---
+++ Compare2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Compare2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Compare2")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b7b8c954-d143-4ebc-b3d5-21a3add82ac9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
